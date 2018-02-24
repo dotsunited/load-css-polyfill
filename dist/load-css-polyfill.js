41eed58669261645f34d421523031304a7970e5c
Jan Sorgalla <jan.sorgalla@dotsunited.de>
2018-02-24 13:23:37 +0100

    Prepare release v1.1.0

--- dist/load-css-polyfill.js
+++ dist/load-css-polyfill.js
@@ -1,4 +1,4 @@
-/*! load-css-polyfill v1.0.0 - Copyright 2017-2018 Dots United GmbH - Licensed MIT */
+/*! load-css-polyfill v1.1.0 - Copyright 2017-2018 Dots United GmbH - Licensed MIT */
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
